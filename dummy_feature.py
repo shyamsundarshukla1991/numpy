913e649f8006736b2cc67335fb76d26fbd4690de
z003hz2y(Shyam Sundar Shukla) <shyam.shukla@siemens.com>
2022-08-17 22:29:29 +0530

    adding new dummy function

---
+++ dummy_feature.py
@@ -0,0 +1,9 @@
+"""
+    :author:
+        Shyam Sundar Shukla
+        CT RDA SDT IST-IN,
+        Siemens Corporate Technology,
+        Bangalore India
+"""
+def dummy_func():
+    print('adding a new dummy function in the numpy open source code')
